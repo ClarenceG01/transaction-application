e9607e51c6d0c4af36349fb5b6095781727c19f9
ClarenceG01 <gatama001@gmail.com>
2023-05-18 16:51:53 +0300

    changes to js

--- index.js
+++ index.js
@@ -1,60 +1,46 @@
-const list=[];
+// Get the form and history container elements
 const form = document.getElementById("form");
-const container = document.querySelector(".history");
-const container_box= document.querySelector(".history-box");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  const user_amount = document.getElementById("amount").value
-  const user_title = document.getElementById("title").value
-  const user_category = document.querySelector('input[name="category"]:checked').value
-  // console.log(user_amount);
-  // console.log(user_title);
-  // console.log(user_category);
+const historyContainer = document.querySelector(".history");
 
-  const new_entry={title:user_title,amount:user_amount,category:user_category}
-  list.push(new_entry)
-//   console.log(new_entry);
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  const amountInput = document.getElementById("amount");
+  const titleInput = document.getElementById("title");
+  const categoryInput = document.querySelector(
+    'input[name="category"]:checked'
+  );
+  const historyBox = document.createElement("div");
+  historyBox.classList.add("history-box");
+
+  historyBox.id = titleInput.value;
+  const titlePara = document.createElement("p");
+  titlePara.classList.add("title");
+  titlePara.textContent = titleInput.value;
+
+  const amountPara = document.createElement("p");
+  amountPara.classList.add("amount");
+  const sign = () => {
+    if (categoryInput.value === "Income") {
+      return "+";
+    } else {
+      return "-";
+    }
+  };
+  amountPara.textContent = sign() + amountInput.value;
+
+  historyBox.appendChild(titlePara);
+  historyBox.appendChild(amountPara);
+  historyContainer.appendChild(historyBox);
+
+  amountInput.value = "";
+  titleInput.value = "";
+  categoryInput.checked = false;
+
+   // //border-radius
+   if (categoryInput.value=== "Income") {
+    historyBox.style.borderRightColor = "green";
+  } else if (categoryInput.value === "Expense") {
+    historyBox.style.borderRightColor = "red";
+  }
   
-  list.forEach(function (transaction, index){
-    const div=document.createElement('div');
-    container.append(div);
-    div.classList.add('history-box', transaction.category );
-    div.setAttribute('data-index', index);
-
-    const titleElement = document.createElement('title');
-    titleElement.classList.add('transaction-title');
-    titleElement.textContent = transaction-title;
-
-    const amountElement = document.createElement('amount');
-    amountElement.classList.add('transaction-amount');
-    amountElement.textContent = transaction.amount;
-
-    div.appendChild(titleElement);
-    div.appendChild(amountElement);
-
-    container_box.appendChild(container);
-  
-
-  })
-  // container_box.innerHTML;
-  // div.innerHTML=container_box.innerHTML;
-  // div.classList.add('history-box')
 });
-// function func() {
-//   addbtn.addEventListener("click", () => {
-//     let user_amount = amount.value;
-//     console.log(user_amount);
-//   });
-// }
-
-// const new_title=document.getElementById('title').value;
-// const new_amount=document.getElementById('amount').value;
-// const new_category=document.querySelector('input[name="category"]:checked')
-
-// add_btn.addEventListener("submit", ()=>{
-//   e.preventDefault();
-//   console.log(new_title);
-//   console.log(new_amount);
-//   console.log(new_category);
-// })
